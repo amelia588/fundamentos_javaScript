0da09e1018be7f237274ab7d554679f063e48afd
Amelia Martel <1473967@senati.pe>
2024-07-31 15:53:37 -0500

    clase de numeros

---
+++ clases_numeros.js
@@ -0,0 +1,27 @@
+// tipo entero y decimal
+const entero = 42;
+const decimal = 3.14;
+console.log(typeof entero, typeof decimal); 
+
+// Notacion cientifica
+const cientifico = 5e3;
+
+// infinitos y NaN
+const infinito = Infinity;
+const noEsUnNumero = NaN;
+
+// Operaciones ariméticas
+// suma, resta, multiplicacion y division
+const suma = 3 + 3;
+const resta = 4 - 4;
+const multiplicacion = 4 * 7;
+const division = 16 / 2;
+
+// Modulo y exponenciacion
+const modulo = 15 % 8; // Corrección aquí
+const exponenciacion = 2 ** 3;
+
+// precision
+const resultado = 0.1 + 0.2;
+console.log(resultado);
+console.log(resultado.toFixed(1));
